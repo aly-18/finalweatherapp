c1fc1674c059668b3c1116a54a118bf7bde5fe9d
aly-18 <122661083+aly-18@users.noreply.github.com>
2023-01-22 22:59:05 +1100

    updated js

--- src/app.js
+++ src/app.js
@@ -1,1 +1,18 @@
-alert("What up");
+function displayTemperature(response) {
+  let temperatureElement = document.querySelector("#current-temp");
+  temperatureElement.innerHTML = Math.round(response.data.temperature.current);
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.city;
+  let descriptionElement = document.querySelector("#description");
+  descriptionElement.innerHTML = response.data.condition.description;
+  let humidityElement = document.querySelector("#humidity");
+  humidityElement.innerHTML = response.data.temperature.humidity + "%";
+  let windElement = document.querySelector("#wind");
+  windElement.innerHTML = Math.round(response.data.wind.speed) + "km/hr";
+}
+let apiUrl =
+  "https://api.shecodes.io/weather/v1/current?query=Melbourne&key=5c0fbb0ef63ff3a8dac04t35bo0ed1e4&units=metric";
+
+console.log(apiUrl);
+
+axios.get(apiUrl).then(displayTemperature);

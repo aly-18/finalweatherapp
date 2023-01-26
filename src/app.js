9a6808d11ac96f62acc9936dd3a9d45979c72430
aly-18 <122661083+aly-18@users.noreply.github.com>
2023-01-26 22:35:56 +1100

    added day

--- src/app.js
+++ src/app.js
@@ -1,9 +1,21 @@
 function formatDate(timestamp) {
   let date = new Date(timestamp);
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
   let hours = date.getHours();
   let minutes = date.getMinutes();
-  let day = date.getDay();
-  return day + hours + minutes;
+  if (minutes < 10) {
+    minutes = "0" + minutes;
+  }
+  return day + " " + hours + ":" + minutes;
 }
 
 function displayTemperature(response) {
@@ -18,23 +30,26 @@ function displayTemperature(response) {
   let windElement = document.querySelector("#wind");
   windElement.innerHTML = Math.round(response.data.wind.speed) + "km/hr";
   let dateElement = document.querySelector("#date");
-  dateElement.innerHTML = formatDate(response.data.time);
+  dateElement.innerHTML = formatDate(response.data.time * 1000);
   let iconElement = document.querySelector("#icon");
   iconElement.setAttribute("src", response.data.condition.icon_url);
 }
 
+function search(city) {
+  let apiUrl =
+    "https://api.shecodes.io/weather/v1/current?query=Melbourne&key=5c0fbb0ef63ff3a8dac04t35bo0ed1e4&units=metric";
+
+  console.log(apiUrl);
+
+  axios.get(apiUrl).then(displayTemperature);
+}
+
+search("paris");
 function handleSearch(event) {
   event.preventDefault();
   let cityInputElement = document.querySelector("#city-input");
   console.log(cityInputElement.value);
 }
-
-let apiUrl =
-  "https://api.shecodes.io/weather/v1/current?query=hawthorn&key=5c0fbb0ef63ff3a8dac04t35bo0ed1e4&units=metric";
-
-console.log(apiUrl);
-
-axios.get(apiUrl).then(displayTemperature);
 
 let form = document.querySelector("form");
 form.addEventListener("submit", handleSearch);

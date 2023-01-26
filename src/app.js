4760f92326245da925f7b174dcc0e1aca758f6f5
aly-18 <122661083+aly-18@users.noreply.github.com>
2023-01-26 12:19:46 +1100

    adding submit form

--- src/app.js
+++ src/app.js
@@ -1,3 +1,11 @@
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  let day = date.getDay();
+  return day + hours + minutes;
+}
+
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#current-temp");
   temperatureElement.innerHTML = Math.round(response.data.temperature.current);
@@ -9,10 +17,24 @@ function displayTemperature(response) {
   humidityElement.innerHTML = response.data.temperature.humidity + "%";
   let windElement = document.querySelector("#wind");
   windElement.innerHTML = Math.round(response.data.wind.speed) + "km/hr";
+  let dateElement = document.querySelector("#date");
+  dateElement.innerHTML = formatDate(response.data.time);
+  let iconElement = document.querySelector("#icon");
+  iconElement.setAttribute("src", response.data.condition.icon_url);
 }
+
+function handleSearch(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  console.log(cityInputElement.value);
+}
+
 let apiUrl =
-  "https://api.shecodes.io/weather/v1/current?query=Melbourne&key=5c0fbb0ef63ff3a8dac04t35bo0ed1e4&units=metric";
+  "https://api.shecodes.io/weather/v1/current?query=hawthorn&key=5c0fbb0ef63ff3a8dac04t35bo0ed1e4&units=metric";
 
 console.log(apiUrl);
 
 axios.get(apiUrl).then(displayTemperature);
+
+let form = document.querySelector("form");
+form.addEventListener("submit", handleSearch);
